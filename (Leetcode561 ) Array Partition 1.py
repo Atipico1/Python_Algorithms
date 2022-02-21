b48b2a71855d11184e17982f02577aa27c3baeaf
Atipico1 <100078632+Atipico1@users.noreply.github.com>
2022-02-22 02:11:13 +0900

    Create (Leetcode561 ) Array Partition 1.py

---
+++ (Leetcode561 ) Array Partition 1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        result = 0
+        for i in range(len(nums)//2):
+            result += nums[2*i]
+        return result
