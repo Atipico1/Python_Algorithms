c238686e0496243c582a5724346d6ad8cb1ff38c
Atipico1 <100078632+Atipico1@users.noreply.github.com>
2022-03-17 20:19:21 +0900

    Create (백준#2178) 미로탐색.py

---
+++ (백준#2178) 미로탐색.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+graph = [ list(map(int, list(input()))) for _ in range(n) ]
+queue = deque([[0, 0]])
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+while queue:
+    x, y = queue.popleft()
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if (0<= nx < n) and (0<= ny < m) and (graph[nx][ny] == 1):
+            queue.append([nx, ny])
+            graph[nx][ny] = graph[x][y] + 1
+print(graph[n-1][m-1])
